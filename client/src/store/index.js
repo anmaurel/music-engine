68eba6e388c9be8db5808ca4341e87bf9cdaf610
Antoine Maurel <antoinemaurel6@gmail.com>
2021-11-23 19:14:50 +0100

    feat: popup notification when playlist is successfully generated

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
+import app from "./modules/app";
 import auth from "./modules/auth";
 import spotify from "./modules/spotify";
 
 export default createStore({
     modules: {
+        app,
         auth,
         spotify,
     },
