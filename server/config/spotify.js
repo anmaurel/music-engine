b031e86b050fbfd9a9065d96f557e4a8a7d1a443
Antoine Maurel <antoinemaurel1@gmail.com>
2021-11-26 11:40:27 +0100

    refactor: add all spotify scopes

--- server/config/spotify.js
+++ server/config/spotify.js
@@ -1,11 +1,11 @@
-import SpotifyWebApi from 'spotify-web-api-node';
+import SpotifyWebApi from "spotify-web-api-node";
 
 export default {
     spotifyAccountsAPI: "https://accounts.spotify.com",
-    scope: "user-read-recently-played user-top-read user-read-playback-state user-library-read user-library-modify playlist-modify-private playlist-read-private playlist-read-collaborative playlist-modify-public",
+    scope: "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private user-read-email user-follow-modify user-follow-read user-library-modify user-library-read streaming app-remote-control user-read-playback-position user-top-read user-read-recently-played playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public",
     api: new SpotifyWebApi({
         clientId: process.env.SPOTIFY_ID,
         clientSecret: process.env.SPOTIFY_SECRET,
         redirectUri: process.env.SPOTIFY_REDIRECT_URL,
-    })
-}
+    }),
+};
